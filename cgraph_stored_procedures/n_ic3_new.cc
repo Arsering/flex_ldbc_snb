ce2b320c1d59e960781cd9d0b00bb56a91703252
zondayc <2645828175@qq.com>
2025-01-13 21:08:51 +0800

    rewrite n_ic3 and n_ic6

---
+++ cgraph_stored_procedures/n_ic3_new.cc
@@ -0,0 +1,431 @@
+#include <queue>
+#include <string_view>
+
+#include "flex/engines/graph_db/app/app_base.h"
+#include "flex/engines/graph_db/database/graph_db_session.h"
+#include "flex/storages/rt_mutable_graph/mutable_property_fragment.h"
+#include "flex/utils/property/types.h"
+// #include "utils.h"
+
+namespace gs
+{
+
+  class IC3 : public AppBase
+  {
+  public:
+    IC3(GraphDBSession &graph)
+        : person_label_id_(graph.schema().get_vertex_label_id("PERSON")),
+          post_label_id_(graph.schema().get_vertex_label_id("POST")),
+          comment_label_id_(graph.schema().get_vertex_label_id("COMMENT")),
+          place_label_id_(graph.schema().get_vertex_label_id("PLACE")),
+          knows_label_id_(graph.schema().get_edge_label_id("KNOWS")),
+          hasCreator_label_id_(graph.schema().get_edge_label_id("HASCREATOR")),
+          isLocatedIn_label_id_(graph.schema().get_edge_label_id("ISLOCATEDIN")),
+          isPartOf_label_id_(graph.schema().get_edge_label_id("ISPARTOF")),
+          post_creationDate_col_(graph.GetPropertyHandle(post_label_id_, "creationDate")),
+          comment_creationDate_col_(graph.GetPropertyHandle(comment_label_id_, "creationDate")),
+          person_firstName_col_(graph.GetPropertyHandle(person_label_id_, "firstName")),
+          person_lastName_col_(graph.GetPropertyHandle(person_label_id_, "lastName")),
+          place_name_col_(graph.GetPropertyHandle(place_label_id_, "name")),
+          place_num_(graph.graph().vertex_num(place_label_id_)),
+          graph_(graph)
+    {
+      place_Locatedin_.resize(place_num_);
+    }
+    ~IC3() {}
+    void get_friends(const ReadTransaction &txn, vid_t root,
+                     std::vector<vid_t> &friends)
+    {
+      std::vector<vid_t> neighbors;
+
+      auto person_knows_person_out = txn.GetOutgoingGraphView<Date>(
+          person_label_id_, person_label_id_, knows_label_id_);
+      auto person_knows_person_in = txn.GetIncomingGraphView<Date>(
+          person_label_id_, person_label_id_, knows_label_id_);
+      auto person_isLocatedIn_place_out =
+          txn.GetOutgoingSingleGraphView<grape::EmptyType>(
+              person_label_id_, place_label_id_, isLocatedIn_label_id_);
+#if OV
+      const auto &ie = person_knows_person_in.get_edges(root);
+      for (auto &e : ie)
+      {
+        neighbors.push_back(e.neighbor);
+      }
+      const auto &oe = person_knows_person_out.get_edges(root);
+      for (auto &e : oe)
+      {
+        neighbors.push_back(e.neighbor);
+      }
+#else
+      auto ie = person_knows_person_in.get_edges(root);
+      for (; ie.is_valid(); ie.next())
+      {
+        neighbors.push_back(ie.get_neighbor());
+      }
+      auto oe = person_knows_person_out.get_edges(root);
+      for (; oe.is_valid(); oe.next())
+      {
+        neighbors.push_back(oe.get_neighbor());
+      }
+#endif
+      friends_.clear();
+      friends_.resize(person_num_, false);
+      if (neighbors.empty())
+      {
+        return;
+      }
+
+      for (auto v : neighbors)
+      {
+#if OV
+        assert(person_isLocatedIn_place_out.exist(v));
+        auto p = person_isLocatedIn_place_out.get_edge(v).neighbor;
+#else
+        auto item = person_isLocatedIn_place_out.get_edge(v);
+        assert(person_isLocatedIn_place_out.exist1(item));
+
+        auto p = gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+        item.free();
+#endif
+        if (!place_Locatedin_[p] && (!friends_[v]))
+        {
+          friends.push_back(v);
+        }
+        friends_[v] = true;
+#if OV
+        const auto &iev = person_knows_person_in.get_edges(v);
+        for (auto &e : iev)
+        {
+          if (!friends_[e.neighbor])
+          {
+            friends_[e.neighbor] = true;
+            assert(person_isLocatedIn_place_out.exist(e.neighbor));
+            auto p = person_isLocatedIn_place_out.get_edge(e.neighbor).neighbor;
+            if (!place_Locatedin_[p])
+            {
+              friends.push_back(e.neighbor);
+            }
+#else
+        auto iev = person_knows_person_in.get_edges(v);
+        for (; iev.is_valid(); iev.next())
+        {
+          if (!friends_[iev.get_neighbor()])
+          {
+            friends_[iev.get_neighbor()] = true;
+            auto item = person_isLocatedIn_place_out.get_edge(iev.get_neighbor());
+            assert(person_isLocatedIn_place_out.exist1(item));
+
+            auto p = gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+            item.free();
+            if (!place_Locatedin_[p])
+            {
+              friends.push_back(iev.get_neighbor());
+            }
+#endif
+          }
+        }
+#if OV
+        const auto &oev = person_knows_person_out.get_edges(v);
+        for (auto &e : oev)
+        {
+          if (!friends_[e.neighbor])
+          {
+            assert(person_isLocatedIn_place_out.exist(e.neighbor));
+            auto p = person_isLocatedIn_place_out.get_edge(e.neighbor).neighbor;
+            if (!place_Locatedin_[p])
+            {
+              friends.push_back(e.neighbor);
+            }
+            friends_[e.neighbor] = true;
+          }
+        }
+#else
+        auto oev = person_knows_person_out.get_edges(v);
+        for (; oev.is_valid(); oev.next())
+        {
+          auto neb = oev.get_neighbor();
+          if (!friends_[neb])
+          {
+            auto item = person_isLocatedIn_place_out.get_edge(neb);
+            assert(person_isLocatedIn_place_out.exist1(item));
+
+            auto p = gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+            if (!place_Locatedin_[p])
+            {
+              friends.push_back(neb);
+            }
+            friends_[neb] = true;
+          }
+        }
+#endif
+      }
+    }
+
+    struct person_info
+    {
+      person_info(int count_, vid_t otherPerson_vid_, oid_t otherPerson_id_)
+          : count(count_),
+            otherPerson_vid(otherPerson_vid_),
+            otherPerson_id(otherPerson_id_) {}
+
+      int count;
+      vid_t otherPerson_vid;
+      oid_t otherPerson_id;
+    };
+
+    struct person_info_comparer
+    {
+      bool operator()(const person_info &lhs, const person_info &rhs)
+      {
+        if (lhs.count > rhs.count)
+        {
+          return true;
+        }
+        if (lhs.count < rhs.count)
+        {
+          return false;
+        }
+        return lhs.otherPerson_id < rhs.otherPerson_id;
+      }
+    };
+
+    bool Query(Decoder &input, Encoder &output) override
+    {
+      // std::cout<<"begin query"<<std::endl;
+      person_count=0;
+      message_count=0;
+      auto txn = graph_.GetReadTransaction();
+
+      oid_t personid = input.get_long();
+      std::string_view countryxname = input.get_string();
+      std::string_view countryyname = input.get_string();
+      int64_t start_date = input.get_long();
+      int durationdays = input.get_int();
+      CHECK(input.empty());
+
+      friends_.clear();
+
+      vid_t root{};
+      if (!txn.GetVertexIndex(person_label_id_, personid, root))
+      {
+        return false;
+      }
+
+      vid_t countryX = place_num_;
+      vid_t countryY = place_num_;
+      for (vid_t i = 0; i < place_num_; ++i)
+      {
+        auto place_name_item = place_name_col_.getProperty(i);
+
+        if (place_name_item == countryxname)
+        {
+          countryX = i;
+        }
+        if (place_name_item == countryyname)
+        {
+          countryY = i;
+        }
+      }
+      assert(countryX != place_num_);
+      assert(countryY != place_num_);
+
+      auto place_isPartOf_place_in=txn.GetIncomingGraphView<grape::EmptyType>(place_label_id_, place_label_id_, isPartOf_label_id_);
+
+      // auto xe = txn.GetIncomingEdges<grape::EmptyType>(
+      //     place_label_id_, countryX, place_label_id_, isPartOf_label_id_);
+      auto xe = place_isPartOf_place_in.get_edges(countryX);
+      for (; xe.is_valid(); xe.next())
+      {
+        place_Locatedin_[xe.get_neighbor()] = true;
+      }
+      // auto ye = txn.GetIncomingEdges<grape::EmptyType>(
+      //     place_label_id_, countryY, place_label_id_, isPartOf_label_id_);
+      auto ye = place_isPartOf_place_in.get_edges(countryY);
+      for (; ye.is_valid(); ye.next())
+      {
+        place_Locatedin_[ye.get_neighbor()] = true;
+      }
+      
+      std::vector<vid_t> friends{};
+      person_num_ = txn.GetVertexNum(person_label_id_);
+      count_.clear();
+      count_.resize(person_num_, {});
+
+      get_friends(txn, root, friends);
+      const int64_t milli_sec_per_day = 24 * 60 * 60 * 1000l;
+      int64_t end_date = start_date + durationdays * milli_sec_per_day;
+      person_info_comparer comparer;
+      std::priority_queue<person_info, std::vector<person_info>,
+                          person_info_comparer>
+          pq(comparer);
+
+      // auto post_hasCreator_person_out =
+      //     txn.GetOutgoingSingleGraphView<grape::EmptyType>(
+      //         post_label_id_, person_label_id_, hasCreator_label_id_);
+      // auto comment_hasCreator_person_out =
+      //     txn.GetOutgoingSingleGraphView<grape::EmptyType>(
+      //         comment_label_id_, person_label_id_, hasCreator_label_id_);
+
+      // auto post_isLocatedIn_place_in=txn.GetIncomingGraphView<grape::EmptyType>(place_label_id_, post_label_id_, isLocatedIn_label_id_);
+      // auto comment_isLocatedIn_place_in=txn.GetIncomingGraphView<grape::EmptyType>(place_label_id_,comment_label_id_,  isLocatedIn_label_id_);
+
+      auto post_hasCreator_person_in = txn.GetIncomingGraphView<grape::EmptyType>(person_label_id_, post_label_id_, hasCreator_label_id_);
+      auto comment_hasCreator_person_in = txn.GetIncomingGraphView<grape::EmptyType>(person_label_id_, comment_label_id_, hasCreator_label_id_);
+      auto post_isLocatedIn_place_out = txn.GetOutgoingSingleGraphView<grape::EmptyType>(post_label_id_, place_label_id_, isLocatedIn_label_id_);
+      auto comment_isLocatedIn_place_out = txn.GetOutgoingSingleGraphView<grape::EmptyType>(comment_label_id_, place_label_id_, isLocatedIn_label_id_);
+
+      for(auto v:friends){
+        auto post_ie=post_hasCreator_person_in.get_edges(v);
+        for(;post_ie.is_valid();post_ie.next()){
+          auto post_id=post_ie.get_neighbor();
+          auto creationDate=gbp::BufferBlock::Ref<Date>(post_creationDate_col_.getProperty(post_id)).milli_second;
+          if(start_date<=creationDate&&creationDate<end_date){
+            auto item=post_isLocatedIn_place_out.get_edge(post_id);
+            assert(post_isLocatedIn_place_out.exist1(item));
+            auto p=gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+            if(p==countryX){
+              count_[v].first++;
+            }else if(p==countryY){
+              count_[v].second++;
+            }
+          }
+        }
+      }
+
+      for(auto v:friends){
+        auto comment_ie=comment_hasCreator_person_in.get_edges(v);
+        for(;comment_ie.is_valid();comment_ie.next()){
+          auto comment_id=comment_ie.get_neighbor();
+          auto creationDate=gbp::BufferBlock::Ref<Date>(comment_creationDate_col_.getProperty(comment_id)).milli_second;
+          if(start_date<=creationDate&&creationDate<end_date){
+            auto item=comment_isLocatedIn_place_out.get_edge(comment_id);
+            assert(comment_isLocatedIn_place_out.exist1(item));
+            auto p=gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+            if(p==countryX){
+              count_[v].first++;
+            }else if(p==countryY){
+              count_[v].second++;
+            }
+          }
+        }
+      }
+
+      for (auto v : friends)
+      {
+        if (count_[v].first && count_[v].second)
+        {
+          int count = count_[v].first + count_[v].second;
+          if (pq.size() < 20)
+          {
+            pq.emplace(count, v, txn.GetVertexId(person_label_id_, v));
+          }
+          else
+          {
+            const person_info &top = pq.top();
+            if (count > top.count)
+            {
+              pq.pop();
+              pq.emplace(count, v, txn.GetVertexId(person_label_id_, v));
+            }
+            else if (count == top.count)
+            {
+              oid_t otherPerson_id = txn.GetVertexId(person_label_id_, v);
+              if (otherPerson_id < top.otherPerson_id)
+              {
+                pq.pop();
+                pq.emplace(count, v, otherPerson_id);
+              }
+            }
+          }
+        }
+      }
+
+      std::vector<person_info> vec;
+      // std::ofstream outfile1;
+      vec.reserve(pq.size());
+      while (!pq.empty())
+      {
+        vec.emplace_back(pq.top());
+        pq.pop();
+      }
+      // outfile1.open("/data-1/yichengzhang/data/latest_gs_bp/graphscope-flex/experiment_space/LDBC_SNB/query_access_log/90_ic3_log",std::ios::app);
+      // outfile1<<vec.size()<<std::endl;
+      for (size_t idx = vec.size(); idx > 0; --idx)
+      {
+        auto &p = vec[idx - 1];
+        auto v = p.otherPerson_vid;
+        output.put_long(p.otherPerson_id);
+
+        auto name = person_firstName_col_.getProperty(v);
+        output.put_buffer_object(name);
+
+        name = person_lastName_col_.getProperty(v);
+        output.put_buffer_object(name);
+
+        output.put_long(count_[v].first);
+        output.put_long(count_[v].second);
+        output.put_long(p.count);
+      }
+      // auto xe_1 = txn.GetIncomingEdges<grape::EmptyType>(
+      //     place_label_id_, countryX, place_label_id_, isPartOf_label_id_);
+      // auto ye_1 = txn.GetIncomingEdges<grape::EmptyType>(
+      //     place_label_id_, countryY, place_label_id_, isPartOf_label_id_);
+      auto xe_1 = place_isPartOf_place_in.get_edges(countryX);
+      for (; xe_1.is_valid(); xe_1.next())
+      {
+        place_Locatedin_[xe_1.get_neighbor()] = false;
+      }
+      auto ye_1 = place_isPartOf_place_in.get_edges(countryY);
+      for (; ye_1.is_valid(); ye_1.next())
+      {
+        place_Locatedin_[ye_1.get_neighbor()] = false;
+      }
+      // std::cout<<"end query"<<std::endl;
+      return true;
+    }
+
+  private:
+    label_t person_label_id_;
+    label_t post_label_id_;
+    label_t comment_label_id_;
+    label_t place_label_id_;
+
+    label_t knows_label_id_;
+    label_t hasCreator_label_id_;
+    label_t isLocatedIn_label_id_;
+    label_t isPartOf_label_id_;
+
+    std::vector<bool> friends_;
+    std::vector<bool> place_Locatedin_;
+    vid_t person_num_;
+    std::vector<std::pair<int, int>> count_;
+
+    cgraph::PropertyHandle post_creationDate_col_;
+    cgraph::PropertyHandle comment_creationDate_col_;
+    cgraph::PropertyHandle person_firstName_col_;
+    cgraph::PropertyHandle person_lastName_col_;
+    cgraph::PropertyHandle place_name_col_;
+
+    vid_t place_num_;
+
+    GraphDBSession &graph_;
+    int person_count;
+    int message_count=0;
+  };
+
+} // namespace gs
+
+extern "C"
+{
+  void *CreateApp(gs::GraphDBSession &db)
+  {
+    gs::IC3 *app = new gs::IC3(db);
+    return static_cast<void *>(app);
+  }
+
+  void DeleteApp(void *app)
+  {
+    gs::IC3 *casted = static_cast<gs::IC3 *>(app);
+    delete casted;
+  }
+}
---
+++ cgraph_stored_procedures/n_ic6_new.cc
@@ -0,0 +1,222 @@
+#include <queue>
+#include <string_view>
+
+#include "flex/engines/graph_db/app/app_base.h"
+#include "flex/engines/graph_db/database/graph_db_session.h"
+#include "flex/storages/rt_mutable_graph/mutable_property_fragment.h"
+#include "flex/utils/property/types.h"
+#include "n_utils.h"
+
+namespace gs
+{
+
+  class IC6 : public AppBase
+  {
+  public:
+    IC6(GraphDBSession &graph)
+        : tag_label_id_(graph.schema().get_vertex_label_id("TAG")),
+          person_label_id_(graph.schema().get_vertex_label_id("PERSON")),
+          post_label_id_(graph.schema().get_vertex_label_id("POST")),
+          knows_label_id_(graph.schema().get_edge_label_id("KNOWS")),
+          hasCreator_label_id_(graph.schema().get_edge_label_id("HASCREATOR")),
+          hasTag_label_id_(graph.schema().get_edge_label_id("HASTAG")),
+          tag_num_(graph.graph().vertex_num(tag_label_id_)),
+          tag_name_col_(graph.GetPropertyHandle(tag_label_id_, "name")),
+          graph_(graph) {}
+
+    ~IC6() {}
+
+    struct tag_info
+    {
+      tag_info(int count_, gbp::BufferBlock tag_name_)
+          : count(count_), tag_name(tag_name_) {}
+      int count;
+      gbp::BufferBlock tag_name;
+    };
+
+    struct tag_info_comparer
+    {
+      bool operator()(const tag_info &lhs, const tag_info &rhs)
+      {
+        if (lhs.count > rhs.count)
+        {
+          return true;
+        }
+        if (lhs.count < rhs.count)
+        {
+          return false;
+        }
+        return lhs.tag_name < rhs.tag_name;
+      }
+    };
+
+    bool Query(Decoder &input, Encoder &output) override
+    {
+      auto txn = graph_.GetReadTransaction();
+
+      oid_t personid = input.get_long();
+      std::string_view tagname = input.get_string();
+      CHECK(input.empty());
+
+      vid_t root{};
+      if (!txn.GetVertexIndex(person_label_id_, personid, root))
+      {
+        return false;
+      }
+      vid_t tag_id = tag_num_;
+      for (vid_t i = 0; i < tag_num_; ++i)
+      {
+        auto tag_name_item = tag_name_col_.getProperty(i);
+        if (tag_name_item == tagname)
+        {
+          tag_id = i;
+          break;
+        }
+      }
+      assert(tag_id != tag_num_);
+
+      get_2d_friends(txn, person_label_id_, knows_label_id_, root,
+                     txn.GetVertexNum(person_label_id_), friends_);
+
+      std::vector<int> post_count(tag_num_, 0);
+
+    //   auto post_hasCreator_person_out =
+    //       txn.GetOutgoingSingleGraphView<grape::EmptyType>(
+    //           post_label_id_, person_label_id_, hasCreator_label_id_);
+      auto post_hasCreator_person_in =
+          txn.GetIncomingGraphView<grape::EmptyType>(
+              person_label_id_, post_label_id_, hasCreator_label_id_);
+      auto posts_with_tag = txn.GetIncomingEdges<grape::EmptyType>(
+          tag_label_id_, tag_id, post_label_id_, hasTag_label_id_);
+      auto post_hasTag_tag_out = txn.GetOutgoingGraphView<grape::EmptyType>(
+          post_label_id_, tag_label_id_, hasTag_label_id_);
+      for(int i=0;i<friends_.size();i++){
+        if(!friends_[i]){
+          continue;
+        }
+        auto post_ie=post_hasCreator_person_in.get_edges(i);
+        for(;post_ie.is_valid();post_ie.next()){
+          auto post_id=post_ie.get_neighbor();
+          auto tag_oe=post_hasTag_tag_out.get_edges(post_id);
+          std::set<vid_t> tag_ids;
+          bool has_tag_id=false;
+          for(;tag_oe.is_valid();tag_oe.next()){
+            auto temp_tag_id=tag_oe.get_neighbor();
+            if(temp_tag_id==tag_id){
+              has_tag_id=true;
+            }
+            tag_ids.insert(temp_tag_id);
+          }
+          if(has_tag_id){
+            for(auto tag_id:tag_ids){
+              ++post_count[tag_id];
+            }
+          }
+        }
+      }
+
+    //   for (; posts_with_tag.is_valid(); posts_with_tag.next())
+    //   {
+    //     vid_t post_id = posts_with_tag.get_neighbor();
+
+    //     auto item = post_hasCreator_person_out.get_edge(post_id);
+    //     assert(post_hasCreator_person_out.exist1(item));
+
+    //     auto creator = gbp::BufferBlock::Ref<gs::MutableNbr<grape::EmptyType>>(item).neighbor;
+    //     if (friends_[creator])
+    //     {
+    //       auto oe = post_hasTag_tag_out.get_edges(post_id);
+    //       for (; oe.is_valid(); oe.next())
+    //       {
+    //         ++post_count[oe.get_neighbor()];
+    //       }
+    //     }
+    //   }
+
+      post_count[tag_id] = 0;
+
+      tag_info_comparer comparer;
+      std::priority_queue<tag_info, std::vector<tag_info>, tag_info_comparer> que(
+          comparer);
+      vid_t other_tag_id = 0;
+      while (que.size() < 10 && other_tag_id < tag_num_)
+      {
+        if (post_count[other_tag_id] > 0)
+        {
+          que.emplace(post_count[other_tag_id],
+                      tag_name_col_.getProperty(other_tag_id));
+        }
+        ++other_tag_id;
+      }
+      while (other_tag_id < tag_num_)
+      {
+        const auto &top = que.top();
+        auto cur_count = post_count[other_tag_id];
+        if (cur_count > top.count)
+        {
+          que.pop();
+          que.emplace(cur_count, tag_name_col_.getProperty(other_tag_id));
+        }
+        else if (cur_count == top.count)
+        {
+          auto tag_name_item = tag_name_col_.getProperty(other_tag_id);
+          if (tag_name_item < top.tag_name)
+          {
+            que.pop();
+            que.emplace(cur_count, tag_name_item);
+          }
+        }
+        ++other_tag_id;
+      }
+      std::vector<tag_info> vec;
+      vec.reserve(que.size());
+      while (!que.empty())
+      {
+        vec.emplace_back(que.top());
+        que.pop();
+      }
+      for (size_t i = vec.size(); i > 0; i--)
+      {
+        output.put_buffer_object(vec[i - 1].tag_name);
+        output.put_int(vec[i - 1].count);
+      }
+
+      // std::ofstream outfile1;
+      // outfile1.open("/data-1/yichengzhang/data/latest_gs_bp/graphscope-flex/experiment_space/LDBC_SNB/query_access_log/100_ic6_log",std::ios::app);
+      // outfile1<<person_count<<"|"<<post_access_num<<"|"<<tag_count<<std::endl;
+      return true;
+    }
+
+  private:
+    label_t tag_label_id_;
+    label_t person_label_id_;
+    label_t post_label_id_;
+
+    label_t knows_label_id_;
+    label_t hasCreator_label_id_;
+    label_t hasTag_label_id_;
+
+    std::vector<bool> friends_;
+
+    vid_t tag_num_;
+    cgraph::PropertyHandle tag_name_col_;
+
+    GraphDBSession &graph_;
+  };
+
+} // namespace gs
+
+extern "C"
+{
+  void *CreateApp(gs::GraphDBSession &db)
+  {
+    gs::IC6 *app = new gs::IC6(db);
+    return static_cast<void *>(app);
+  }
+
+  void DeleteApp(void *app)
+  {
+    gs::IC6 *casted = static_cast<gs::IC6 *>(app);
+    delete casted;
+  }
+}
